2c3906718cace6ed572f4989665c8b685c97e225
Tobias Günther <tg@fournova.com>
2015-05-15 12:30:51 +0200

    Put "about-us" and "footer" into their own snippets

---
+++ site/snippets/about-us.php
@@ -0,0 +1,12 @@
+<div class="row-wrapper">
+  <div class="outer-container">
+    <section id="about">
+      <h5>About Us</h5>
+      <h3>Tower - Not a moustache, but the best Git client.</h3>
+      <div class="tower-icon"></div>
+      <p>We help over 40,000 users in companies like Apple, Google, Amazon, Twitter, and Facebook to easily &amp; productively work with the Git version control system.</p>
+        <a class="button button--try" href="http://www.git-tower.com">Try it 30 days for free</a>
+      </p>
+    </section>
+  </div>
+</div>
---
+++ site/snippets/footer.php
@@ -0,0 +1,5 @@
+<div class="row-wrapper">
+  <div class="outer-container">
+    <footer>&copy; 2015 <a href="http://www.fournova.com">fournova</a></footer>
+  </div>
+</div>
