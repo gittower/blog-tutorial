e7378ac8d505aa068acfb33916acc7c0c8d91f82
Tobias Günther <tg@fournova.com>
2015-05-15 18:29:06 +0200

    Display very latest post, including teaser image

---
+++ site/snippets/newest-post.php
@@ -0,0 +1,17 @@
+<?php
+
+// Get all child pages in the "posts" page
+$allPosts = $pages->get('posts')->children()->visible();
+
+// we want only the latest item
+$newestPost = $allPosts->last();
+
+?>
+
+<article class="teaser--top">
+  <div class="meta"><?php echo date("M d, Y", $newestPost->date()) ?> by <?php echo $newestPost->author() ?></div>
+  <h1><?php echo $newestPost->title() ?></h1>
+  <div class="teaser-image"><img src="<?php echo $newestPost->image('teaser.jpg')->url() ?>" /></div>
+  <p class="teaser-content"><?php echo $newestPost->text()->kirbytext()->excerpt(200) ?></p>
+  <p class="continue-reading"><a href="<?php echo $newestPost->url() ?>">Continue Reading</a></p>
+</article>
--- site/templates/home.php
+++ site/templates/home.php
@@ -4,13 +4,7 @@
   
     <div class="row-wrapper">
       <div class="outer-container">
-        <article class="teaser--top">
-          <div class="meta">Mar 10, 2015 by Ralph Grando</div>
-          <h1>The Chevron</h1>
-          <div class="teaser-image"></div>
-          <p class="teaser-content">It's fair to say that The Chevron ruled the culture of the 1980s. No less a figure than Tom Selleck himself made it popular in our all-time favorite TV series Magnum. In this edition, we'll take a closer look at this fine portion of facial hair.</p>
-          <p class="continue-reading"><a href="detail-view.html">Continue Reading</a></p>
-        </article>
+        <?php snippet("newest-post") ?>
       </div>
     </div>
     
