2d828bfcbfd6a774bddccee0daba2b36ae4cd8a3
Tobias Günther <tg@fournova.com>
2015-05-15 11:57:32 +0200

    Add template assets and static HTML template for home.php

---
+++ site/templates/home.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>The Moustache Monthly</title>
+
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui" />
+    
+    <link rel="stylesheet" href="assets/css/styles.css" />
+  </head>
+
+
+  <body>
+
+    <div class="row-wrapper">
+      <div class="outer-container">
+        <header>
+          <div id="logo">
+            <img src="assets/images/logo.svg" />
+          </div>
+          <nav>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+          </nav>
+        </header>
+      </div>
+    </div>
+  
+    <div class="row-wrapper">
+      <div class="outer-container">
+        <article class="teaser--top">
+          <div class="meta">Mar 10, 2015 by Ralph Grando</div>
+          <h1>The Chevron</h1>
+          <div class="teaser-image"></div>
+          <p class="teaser-content">It's fair to say that The Chevron ruled the culture of the 1980s. No less a figure than Tom Selleck himself made it popular in our all-time favorite TV series Magnum. In this edition, we'll take a closer look at this fine portion of facial hair.</p>
+          <p class="continue-reading"><a href="detail-view.html">Continue Reading</a></p>
+        </article>
+      </div>
+    </div>
+    
+    <div class="row-wrapper">
+      <div class="outer-container">
+        <section id="recent-posts">
+          <article class="teaser--recent">
+            <div class="meta">Feb 25, 2015 by Ralph Grando</div>
+            <h2>The Pencil</h2>
+            <p class="teaser-content">It's narrow, straight and thin as if drawn on by a pencil. It chose Clark Gable as its host and conquered the world of the 1940s quicker than the flu.</p>
+            <p class="continue-reading"><a href="detail-view.html">Continue Reading</a></p>
+          </article>
+      
+          <article class="teaser--recent">
+            <div class="meta">Jan 19, 2015 by Ralph Grando</div>
+            <h2>The Handlebar</h2>
+            <p class="teaser-content">What's bushy and has small upward pointing edges? Correct! The one and only "Handlebar".</p>
+            <p class="continue-reading"><a href="detail-view.html">Continue Reading</a></p>
+          </article>
+      
+          <article class="teaser--recent">
+            <div class="meta">Dec 9, 2014 by Ralph Grando</div>
+            <h2>The Dalí</h2>
+            <p class="teaser-content">Famous painter Salvador Dalí not only had a hand for the canvas. His moustache also made history.</p>
+            <p class="continue-reading"><a href="detail-view.html">Continue Reading</a></p>
+          </article>
+      
+          <article class="teaser--recent">
+            <div class="meta">Nov 17, 2014 by Ralph Grando</div>
+            <h2>Fu Manchu</h2>
+            <p class="teaser-content">Without the Fu Manchu, our stereotypes of ancient Asia would simply not be complete.</p>
+            <p class="continue-reading"><a href="detail-view.html">Continue Reading</a></p>
+          </article>
+        </section>
+
+        <section id="subscribe">
+          <div class="note">
+            <h3>Don't miss the next moustache!</h3>
+            <p>Follow us on <a href="http://www.twitter.com/gittower">Twitter</a> or subscribe via <a href="#">RSS</a>.</p>
+          </div>
+          <div class="follow">
+            <a href="#" class="button button--twitter"></a>
+            <a href="#" class="button button--rss">Subscribe</a>
+          </div>
+        </section>
+      </div>
+    </div>
+    
+    <div class="row-wrapper">
+      <div class="outer-container">
+        <section id="about">
+          <h5>About Us</h5>
+          <h3>Tower - Not a moustache, but the best Git client.</h3>
+          <div class="tower-icon"></div>
+          <p>We help over 40,000 users in companies like Apple, Google, Amazon, Twitter, and Facebook to easily &amp; productively work with the Git version control system.</p>
+            <a class="button button--try" href="http://www.git-tower.com">Try it 30 days for free</a>
+          </p>
+        </section>
+      </div>
+    </div>
+    
+    <div class="row-wrapper">
+      <div class="outer-container">
+        <footer>&copy; 2015 <a href="http://www.fournova.com">fournova</a></footer>
+      </div>
+    </div>
+    
+  </body>
+</html>
