98ed72bfe9fb11cb527ee26fe5ce965009b16471
Tobias Günther <tg@fournova.com>
2015-05-15 12:23:53 +0200

    Change site metadata and fill our header snippet

--- site/snippets/html-header.php
+++ site/snippets/html-header.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>The Moustache Monthly</title>
+    <title><?php echo $site->title() ?></title>
 
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui" />
+    <meta name="description" content="<?php echo $site->description() ?>" />
+    <meta name="keywords" content="<?php echo $site->keywords() ?>" />
     
     <link rel="stylesheet" href="assets/css/styles.css" />
   </head>
