cf90f759439c0fbb60792e8d8aca755992a6a6ae
Tobias Günther <tg@fournova.com>
2015-05-18 09:44:11 +0200

    Default template, with static HTML content

---
+++ site/templates/default.php
@@ -0,0 +1,45 @@
+<?php snippet("html-header"); ?>
+<?php snippet("navigation"); ?>
+
+    <div class="row-wrapper">
+      <div class="outer-container">
+        <article id="full-article">
+          <div id="main-image"></div>
+          <div id="article-content">
+            <div class="meta">Mar 10, 2015 by Ralph Grando</div>
+            <h1>The Chevron</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </p>
+            <p>lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div class="content-image"></div>
+            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </article>
+      </div>
+    </div>
+    
+    <div class="row-wrapper">
+      <div class="outer-container">
+        <section id="author">
+          <div class="author-icon"></div>
+          <div class="author-content">
+            <h5>About Ralph Grando</h5>
+            <p>A man of style, Ralph grew his first moustache in the tender age of 12. Over the years, he has become what can safely be called a "connoisseur" of facial hair.</p>
+          </div>
+        </section>
+
+        <section id="recommended-reading">
+          <h5>Recommended Reading</h5>
+          <ul>
+            <li><a href="#">Trimming &amp; Clipping: The Basic Techniques</a></li>
+            <li><a href="#">An Ode to the Moustache</a></li>
+            <li><a href="#">The Horseshoe: A Grown Man's Beard</a></li>
+            <li><a href="#">All Articles</a></li>
+          </ul>
+        </section>
+      </div>
+    </div>
+
+<?php snippet("footer"); ?>
+<?php snippet("html-footer"); ?>
