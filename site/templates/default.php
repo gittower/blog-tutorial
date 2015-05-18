b3e83f09cd37e253522086e39094712dadb2352d
Tobias Günther <tg@fournova.com>
2015-05-18 11:26:13 +0200

    Replace static content in post header
    Accessing a "teaser.jpg" image file and the post's textual & meta content

--- site/templates/default.php
+++ site/templates/default.php
@@ -4,16 +4,15 @@
     <div class="row-wrapper">
       <div class="outer-container">
         <article id="full-article">
-          <div id="main-image"></div>
+          <div id="main-image">
+            <?php if($page->image('teaser.jpg')){
+              echo '<img src="'. $page->image('teaser.jpg')->url() .'" />';
+            } ?>
+          </div>
           <div id="article-content">
-            <div class="meta">Mar 10, 2015 by Ralph Grando</div>
-            <h1>The Chevron</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  </p>
-            <p>lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <div class="content-image"></div>
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div class="meta"><?php echo date("M d, Y", $page->date()) ?> by <?php echo $page->author() ?></div>
+            <h1><?php echo $page->title() ?></h1>
+            <?php echo $page->text()->kirbytext() ?>
           </div>
         </article>
       </div>
